864da331d3c987495dbf658489b67fb63b2f64d9
Ricardo Araujo <ricardoluisaraujo2006@gmail.com>
2020-08-21 18:00:34 +0100

    idk what im doing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+puts "Destroy ingredients"
+Ingredient.destroy_all if Rails.env.development?
+
+puts "Destroy Cocktails"
+Cocktail.destroy_all
+
+
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
 
-Cocktail.create(name: "Strawberry Flavor")
-Cocktail.create(name: "Melon Flavor")
-Cocktail.create(name: "Whatever Flavor")
+# straw = Cocktail.new(name: "Starberry Cocktail")
+# straw.save
+# olive = Cocktail.new(name: "Olive Cocktail")
+# olive.save
+lemonade = Cocktail.new(name: "Lemonade Cocktail")
+lemonade.save
+puts lemonade
+
+puts "Create ingredients"
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+ingredients["drinks"].each do |ingredient|
+  i = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "create #{i.name}"
+end
+
+puts "Creating cocktails"
+30.times do
+  cocktail = Cocktail.create(
+    name: Faker::JapaneseMedia::DragonBall.character
+    )
+  puts cocktail
+end
